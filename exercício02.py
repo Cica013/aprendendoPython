326b0b0f1c2ae0be109795914a77ee839e6698ca
SilvanisR.S <61808853+Cica013@users.noreply.github.com>
2021-07-14 19:04:29 -0300

    Iniciando exercício02, localizando valores duplicados em listas.

--- exercício02.py
+++ exercício02.py
@@ -1,3 +1,17 @@
+"""
+É uma lista de números inteiros; As listas internas tem o tamanho de 10 elementos As listas internas contém
+números entre 1 a 10 e eles podem ser duplicados
+
+Exercício
+
+Crie uma função que encontrao primeiro duplicado considerando o segundo número como a duplicação.
+Retorne a duplicação considerada. Requisitos: A ordem do número duplicado é considerada a partir da
+segunda ocorrência do número, ou seja, o número duplixado em si. Exemplo:
+[1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3) [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
+
+Se não encontrar duplicados na lista, retorne -1
+"""
+
 lista_de_listas_de_inteiros = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
