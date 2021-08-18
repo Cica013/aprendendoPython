fd6b63c8d80f7a4b5bbf3c2796a3588fc1269e66
SilvanisR.S <61808853+Cica013@users.noreply.github.com>
2021-08-18 18:43:05 -0300

    Aprendendo a trabalhar com classes em Python.

---
+++ aprendendo_classes.py
@@ -0,0 +1,15 @@
+from produtos01 import Notebook
+
+pc = Notebook(nome_notebook='Samsung Odyssey 7', marca='Samsung', processador='i7 10°G', ram='16',
+              tipo_armazenamento='ssd', qtd_armazenamento='850 Gb', gpu='GForce GTX NVidia 1650 Ti',
+              monitor='15,5 Polegadas', cor='Azul Escuro')
+
+pc.ligar()
+pc.ligar()
+pc.desligar()
+pc.desligar()
+pc.exibir_especificacoes()
+pc.ligar()
+print('Uma Ótima Máquina!!!')
+
+
---
+++ produtos01.py
@@ -0,0 +1,36 @@
+class Notebook:
+    fonte = 'fonte de 30W'
+    def __init__(self, nome_notebook, marca, processador, ram, tipo_armazenamento, qtd_armazenamento, gpu, monitor,
+                 cor, ligar_desligar=False, ):
+        self.nome_notebook = nome_notebook
+        self.marca = marca
+        self.processador = processador
+        self.ram = ram
+        self.tipo_armazenamento = tipo_armazenamento
+        self.qtd_armazenamento = qtd_armazenamento
+        self.gpu = gpu
+        self.monitor = monitor
+        self.cor = cor
+        self.ligar_desligar = ligar_desligar
+
+    def ligar(self):
+        if self.ligar_desligar:
+            print('Já estou ligado!')
+            return
+        print('Estou ligando...')
+        self.ligar_desligar = True
+
+    def desligar(self):
+        if not self.ligar_desligar:
+            print('Já estou desligado!')
+            return
+        print('Desligando...')
+        self.ligar_desligar = False
+
+    def exibir_especificacoes(self):
+        print(
+            '\033[1;4;3;32mEspecificações técnicas:\033[m \n'
+            f'Modelo:\t\t{self.nome_notebook},\nMarca:\t\t{self.marca},\nProcessador:\t\t{self.processador},\n'
+            f'Memória ram:\t\t{self.ram},\nTipo armazenamento:\t\t{self.tipo_armazenamento},'
+            f'\nQtd armazenamento:\t\t{self.qtd_armazenamento},\nPlaca de vídeo:\t\t{self.gpu},'
+            f'\nMonitor:\t\t{self.monitor},\nCor:\t\t{self.cor}')
