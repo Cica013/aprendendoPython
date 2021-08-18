10c7004922c504f484f15b456eab7d522aa325e7
SilvanisR.S <61808853+Cica013@users.noreply.github.com>
2021-08-18 18:51:50 -0300

    Corrigindo exercício 07 (validação cnpj)

--- Exercício07/cnpj.py
+++ Exercício07/cnpj.py
@@ -1,84 +1,77 @@
-def formata_cnpj(cnpj):
-    cnpj = cnpj.replace('-', '')
-    cnpj = cnpj.replace('.', '')
-    cnpj = cnpj.replace('/', '')
-    cnpj = cnpj[:-2]
-    cnpj = list(cnpj)
-    new_cnpj = []
-    for x in cnpj:
-        new_cnpj.append(int(x))
-    return new_cnpj
+import re
+import random
 
-
-def formata_cnpj_original(cnpj):
-    cnpj = cnpj.replace('-', '')
-    cnpj = cnpj.replace('.', '')
-    cnpj = cnpj.replace('/', '')
-    cnpj = list(cnpj)
-    new_cnpj = []
-    for x in cnpj:
-        new_cnpj.append(int(x))
-    return new_cnpj
-
-
-def contagem_5():
-    lista = []
-    for x in range(5, 1, -1):
-        lista.append(x)
-    for y in range(9, 1, -1):
-        lista.append(y)
-    return lista
-
-
-def contagem_6():
-    lista = []
-    for x in range(6, 1, -1):
-        lista.append(x)
-    for y in range(9, 1, -1):
-        lista.append(y)
-    return lista
-
-
-def fazendo_validacao(cnpj):
-    cnpj = formata_cnpj(cnpj)
-    cont_5 = contagem_5()
-    soma = 0
-    for x, y in zip(cnpj, cont_5):
-        soma += x * y
-    formula = 11 - (soma % 11)
-    if formula > 9:
-        cnpj.append(0)
-    else:
-        cnpj.append(formula)
-    cont_6 = contagem_6()
-    soma = 0
-    formula = 0
-    for x, y in zip(cnpj, cont_6):
-        soma += x * y
-    formula = soma
-    if formula > 9:
-        cnpj.append(0)
-    else:
-        cnpj.append(formula)
-
-    return cnpj
-
-
-def confirma_validacao(cnpj_1, cnpj_2):
-    if cnpj_1 == cnpj_2:
-        print('O cnpj é VÁLIDO!')
-    else:
-        print('O cnpj é INVÁLIDO!')
+REGRESSIVOS = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 
 
 def valida_cnpj(cnpj):
-    cnpj_recebido = formata_cnpj_original(cnpj)
-    cnpj_validado = fazendo_validacao(cnpj)
-    print(f'cnpj que foi passado: {cnpj_recebido}')
-    print(f'cnpj verificado: {cnpj_validado}')
-    confirma_validacao(cnpj_recebido, cnpj_validado)
+    cnpj = apenas_numeros(cnpj)
+    if sequenc(cnpj):
+        print('É uma sequência')
+        return False
+
+    novo_cnpj = calcula_digito(cnpj=cnpj, digito=1)
+    novo_cnpj = calcula_digito(cnpj=novo_cnpj, digito=2)
+
+    if novo_cnpj == cnpj:
+        return True
+    else:
+        return False
 
 
+def calcula_digito(cnpj, digito):
+    global regressivos
+    if digito == 1:
+        regressivos = REGRESSIVOS[1:]
+        novo_cnpj = cnpj[:-2]
+    elif digito == 2:
+        regressivos = REGRESSIVOS
+        novo_cnpj = cnpj
+    else:
+        return None
+
+    total = 0
+    for indice, regressivo in enumerate(regressivos):
+        total += int(cnpj[indice]) * regressivo
+
+    digito = 11 - (total % 11)
+    digito = digito if digito <= 9 else 0
+
+    return f'{novo_cnpj}{digito}'
+
+
+def sequenc(cnpj):
+    sequencia = cnpj[0] * len(cnpj)
+
+    if sequencia == cnpj:
+        return True
+    else:
+        return False
+
+
+def apenas_numeros(cnpj):
+    return re.sub(r'[^0-9]', '', cnpj)
+
+
+def gera():
+    primeiro_digito = random.randint(0, 9)
+    segundo_digito = random.randint(0, 9)
+    segundo_bloco = random.randint(100, 999)
+    terceiro_bloco = random.randint(100, 999)
+    quarto_bloco = '0001'
+
+    inicio_cnpj = f'{primeiro_digito}{segundo_digito}{segundo_bloco}{terceiro_bloco}{quarto_bloco}00'
+
+    novo_cnpj1 = calcula_digito(cnpj=inicio_cnpj, digito=1)
+    novo_cnpj = calcula_digito(cnpj=novo_cnpj1, digito=2)
+
+    return novo_cnpj
+
+
+def formata_cnpj(cnpj):
+    cnpj = apenas_numeros(cnpj)
+    formatado = f'{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}'
+    return formatado
 
 
 
