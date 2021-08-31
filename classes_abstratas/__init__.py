6c57544fed4d5be4d81c959cff059886196db7cc
SilvanisR.S <61808853+Cica013@users.noreply.github.com>
2021-08-30 21:01:52 -0300

    Estudo sobre: Polimorfismo

---
+++ classes_abstratas/__init__.py
@@ -0,0 +1,15 @@
+from cp import ContaPoupanca
+from cc import ContaCorrente
+
+cp = ContaPoupanca(1111, 2222, 0)
+cp.depositar(10)
+cp.sacar(5)
+cp.sacar(5)
+cp.sacar(1)
+
+print('#' * 40)
+cc = ContaCorrente(agencia=1111, conta=2222, saldo=0, limite=500)
+cc.depositar(100)
+cc.sacar(250)
+cc.sacar(500)
+cc.depositar(1000)
