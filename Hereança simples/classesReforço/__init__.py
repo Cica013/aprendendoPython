a48e8c008f1367ad7966bc47144e72a26089f2e7
SilvanisR.S <61808853+Cica013@users.noreply.github.com>
2021-09-01 20:32:56 -0300

    Estudo de reforço sobre herança simples.

---
+++ Hereança simples/classesReforço/__init__.py
@@ -0,0 +1,12 @@
+"""
+Biblioteca -> chama_interface
+    Interface(Biblioteca) -> metodo_da_interface
+    metodo_da_interface()
+"""
+
+
+from interface import Interface
+
+i1 = Interface()
+i1.metodo_da_interface()
+i1.chama_metodo_da_interface()
