22378eb5ccfe7150b36bc59bb8d1baab222aba4e
SilvanisR.S <61808853+Cica013@users.noreply.github.com>
2021-09-13 20:11:18 -0300

    Datetime 02

--- Datetime/__init__.py
+++ Datetime/__init__.py
@@ -1,5 +1,7 @@
 # https://docs.python.org/3.0/library/datetime.html
 from datetime import datetime, timedelta
+from locale import setlocale, LC_ALL
+from calendar import mdays
 
 # strptime(str, fmt)
 # .strftime(fmt)
@@ -8,11 +10,17 @@ from datetime import datetime, timedelta
 # data = datetime.fromtimestamp(1631569996.0)
 # print(data.strftime('%d/%m/%Y %H:%M:%S'))
 
-d1 = datetime.strptime('20/04/1987 20:00:00', '%d/%m/%Y %H:%M:%S')
-d2 = datetime.strptime('25/04/1987 23:30:00', '%d/%m/%Y %H:%M:%S')
+setlocale(LC_ALL, 'pt_BR.utf-8')
+
+dt = datetime.now()
+mes_atual = int(dt.strftime('%m'))
+formatacao1 = dt.strftime('%A, %d de %B de %Y')
+formatacao2 = dt.strftime('%d/%m/%Y %H:%M:%S')
+print(formatacao1)
+print(formatacao2)
+print(mes_atual, mdays[mes_atual])
+
 # dif = d2 - d1
 # print(dif.days)
-print(d2 > d1)
-print(d1.time())
-
-
+# print(d2 > d1)
+# print(d1.time())
