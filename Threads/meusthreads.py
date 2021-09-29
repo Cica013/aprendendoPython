51c5304743d79157083c275f30b4615ab0b44682
Silvanis <silvanis.silva10@gmail.com>
2021-09-29 11:47:02 -0300

    Aprendendo sobre Threads em Python.

--- Threads/meusthreads.py
+++ Threads/meusthreads.py
@@ -1,5 +1,5 @@
 from time import sleep
-from threading import Thread
+from threading import Thread, Lock
 
 
 # class MeuThread(Thread):
@@ -46,3 +46,77 @@ from threading import Thread
 #     print(i, end=' ')
 #     sleep(.5)
 ##################################################################
+
+# def vai_demorar(texto, tempo):
+#     sleep(tempo)
+#     print(texto)
+#
+#
+# t1 = Thread(target=vai_demorar, args=('Olá mundo 1!', 2))
+# t1.start()
+# t1.join()
+#
+# print('Thread acabou!')
+########################################################################
+
+class Ingressos:
+    # classe que vende ingressos
+
+    def __init__(self, estoque):
+        # Inicializando...
+        # Estoque: quantidade de ingressos em estoque.
+
+        self.estoque = estoque
+        # Nosso cadeado
+        self.lock = Lock()
+
+    def comprar(self, quantidade):
+        """
+        Compra determinada quantidade de ingressos
+
+        :param quantidade: A quantidade de ingressos que deseja comprar.
+        :type quantidade: int
+        :return: Nada
+        :rtype: None
+        """
+        # Tranca o método
+        self.lock.acquire()
+
+        if self.estoque < quantidade:
+            print('Não temos ingressos suficientes.')
+            # Libera o método
+            self.lock.release()
+            return
+        sleep(1)
+
+        self.estoque -= quantidade
+        print(f'Você comporou {quantidade} ingresso(s).'
+              f' Ainda temos {self.estoque} em estoque.')
+
+        # Libera o método
+        self.lock.release()
+
+
+if __name__ == '__main__':
+    ingressos = Ingressos(10)
+
+    threads = []  # Lista para manter as threads
+    for i in range(1, 20):
+        t = Thread(target=ingressos.comprar, args=(i,))
+        threads.append(t)
+
+    # Inicia as threads
+    for t in threads:
+        t.start()
+
+        # Verifica se todas as threads terminaram
+    executando = True
+    while executando:
+        executando = False
+
+        for t in threads:
+            if t.is_alive():
+                executando = True
+                break
+
+    print(ingressos.estoque)
