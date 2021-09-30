844a7c6f59c705880802cf7b128ffad0152cd9ee
Silvanis <silvanis.silva10@gmail.com>
2021-09-30 14:26:45 -0300

    Funcinou... provavelmente foi problema de eu ter colocado o mesmo nome do arquivo com o nome do módulo que eu importei, eu mudei e parece que agora funcinou.

--- Manipulando_pdfs_em_python/manipula_pdf.py
+++ Manipulando_pdfs_em_python/manipula_pdf.py
@@ -8,34 +8,34 @@ pip install pypdf2 # virtualenv
 pipenv install pypdf2 # pipenv
 """
 import PyPDF2
-import os
+# import os
 
 
-# JUNTAS PDFs
-caminho_dos_pdfs = 'pdf'
-
-novo_pdf = PyPDF2.PdfFileMerger()
-for root, dirs, files in os.walk(caminho_dos_pdfs):
-    for file in files:
-        caminho_completo_arquivo = os.path.join(root, file)
-
-        arquivo_pdf = open(caminho_completo_arquivo, 'rb')
-        novo_pdf.append(arquivo_pdf)
-
-with open(fr'{caminho_dos_pdfs}\novo_arquivo.pdf', 'wb') as meu_novo_pdf:
-    novo_pdf.write(meu_novo_pdf)
-
-
-
-# # SEPARA PDFs
-# with open('pdf/novo_arquivo.pdf', 'rb') as arquivo_pdf:
-#     leitor = PyPDF2.PdfFileReader(arquivo_pdf)
-#     num_paginas = leitor.getNumPages()
+# # JUNTAS PDFs
+# caminho_dos_pdfs = 'pdf'
 #
-#     for num_pagina in range(num_paginas):
-#         escritor = PyPDF2.PdfFileWriter()
-#         pagina_atual = leitor.getPage(num_pagina)
-#         escritor.addPage(pagina_atual)
+# novo_pdf = PyPDF2.PdfFileMerger()
+# for root, dirs, files in os.walk(caminho_dos_pdfs):
+#     for file in files:
+#         caminho_completo_arquivo = os.path.join(root, file)
 #
-#         with open(f'novos_pdfs/{num_pagina}.pdf', 'wb') as novo_pdf:
-#             escritor.write(novo_pdf)
+#         arquivo_pdf = open(caminho_completo_arquivo, 'rb')
+#         novo_pdf.append(arquivo_pdf)
+#
+# with open(fr'{caminho_dos_pdfs}\novo_arquivo.pdf', 'wb') as meu_novo_pdf:
+#     novo_pdf.write(meu_novo_pdf)
+
+
+
+# SEPARA PDFs
+with open(r'c:pdf\novo_arquivo.pdf', 'rb') as arquivo_pdf:
+    leitor = PyPDF2.PdfFileReader(arquivo_pdf)
+    num_paginas = leitor.getNumPages()
+
+    for num_pagina in range(num_paginas):
+        escritor = PyPDF2.PdfFileWriter()
+        pagina_atual = leitor.getPage(num_pagina)
+        escritor.addPage(pagina_atual)
+
+        with open(fr'novos_pdfs\{num_pagina}.pdf', 'wb') as novo_pdf:
+            escritor.write(novo_pdf)
